5a04d7691a1ed921bdb6346d199bd9487fb20b91
Renektonn <59362328+Renektonn@users.noreply.github.com>
2021-08-05 21:23:44 +0800

    Add files via upload

---
+++ Java2DGame - 複製/TheBreakoutgame/Brick.java
@@ -0,0 +1,38 @@
+import javax.swing.ImageIcon;
+
+public class Brick extends Sprite {
+
+    private boolean destroyed;
+
+    public Brick(int x, int y) {
+
+        initBrick(x, y);
+    }
+
+    private void initBrick(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+
+        destroyed = false;
+
+        loadImage();
+        getImageDimensions();
+    }
+
+    private void loadImage() {
+
+        ImageIcon ii = new ImageIcon("C:/Users/user/Desktop/Java2DGame/images/breakout/brickie.png");
+        image = ii.getImage();
+    }
+
+    boolean isDestroyed() {
+
+        return destroyed;
+    }
+
+    void setDestroyed(boolean val) {
+
+        destroyed = val;
+    }
+}
